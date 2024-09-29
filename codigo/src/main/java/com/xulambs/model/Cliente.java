8e0f16ee214e5f12c9855c69c664afea169d5258
Débora <144275722+debora67@users.noreply.github.com>
2024-09-29 16:04:41 -0300

    Update Cliente.java

--- codigo/src/main/java/com/xulambs/model/Cliente.java
+++ codigo/src/main/java/com/xulambs/model/Cliente.java
@@ -1,15 +1,64 @@
 package com.xulambs.model;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Cliente {
     private String nome;
-    private List<Veiculo> veiculos;
+    private String cpf;  // Alterado de "identificador" para "cpf"
+    private ArrayList<Veiculo> veiculos;
+    private boolean anonimo;
 
-    public Cliente(String nome) {
+    public Cliente(String nome, String cpf) {
         this.nome = nome;
+        this.cpf = cpf;
         this.veiculos = new ArrayList<>();
+        this.anonimo = false;
+    }
+
+    public Cliente() {
+        this.nome = "Anônimo";
+        this.cpf = "00000000000";  // Exemplo de CPF fictício para cliente anônimo
+        this.veiculos = new ArrayList<>();
+        this.anonimo = true;
+    }
+
+    public void adicionarVeiculo(Veiculo veiculo) {
+        veiculos.add(veiculo);
+        System.out.println("Veículo " + veiculo.getPlaca() + " adicionado ao cliente " + nome);
+    }
+
+    public void removerVeiculo(Veiculo veiculo) {
+        if (veiculos.contains(veiculo)) {
+            veiculos.remove(veiculo);
+            System.out.println("Veículo " + veiculo.getPlaca() + " removido do cliente " + nome);
+        } else {
+            System.out.println("O cliente não possui esse veículo.");
+        }
+    }
+
+    public boolean isAnonimo() {
+        return anonimo;
+    }
+
+    public void ocuparVaga(Estacionamento estacionamento, Vaga vaga, Veiculo veiculo) {
+        if (veiculos.contains(veiculo)) {
+            if (estacionamento.ocuparVaga(vaga, veiculo)) {
+                System.out.println("Veículo " + veiculo.getPlaca() + " ocupou a vaga " + vaga.getIdentificador());
+            } else {
+                System.out.println("A vaga " + vaga.getIdentificador() + " já está ocupada.");
+            }
+        } else {
+            System.out.println("O veículo " + veiculo.getPlaca() + " não está registrado para o cliente " + nome);
+        }
+    }
+
+    public void liberarVaga(Estacionamento estacionamento, Vaga vaga) {
+        if (vaga.isOcupada()) {
+            estacionamento.liberarVaga(vaga);
+            System.out.println("Vaga " + vaga.getIdentificador() + " foi liberada.");
+        } else {
+            System.out.println("A vaga " + vaga.getIdentificador() + " já está desocupada.");
+        }
     }
 
     public String getNome() {
@@ -20,20 +69,15 @@ public class Cliente {
         this.nome = nome;
     }
 
-    public List<Veiculo> getVeiculos() {
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public ArrayList<Veiculo> getVeiculos() {
         return veiculos;
-    }
-
-    public void adicionarVeiculo(Veiculo veiculo) {
-        veiculos.add(veiculo);
-    }
-
-    public Veiculo possuiVeiculoEstacionado(String placa) {
-        for (Veiculo veiculo : veiculos) {
-            if (veiculo.getPlaca().equals(placa)) {
-                return veiculo;
-            }
-        }
-        return null;
     }
 }
