8ab2f4350ac6adf69138189641d86ba86dd67d84
JoaoFRAPorto <167324275+JoaoFRAPorto@users.noreply.github.com>
2024-11-03 20:31:13 -0300

    Update Main.java

--- codigo/src/main/java/com/xulambs/Main.java
+++ codigo/src/main/java/com/xulambs/Main.java
@@ -2,6 +2,8 @@ import com.xulambs.model.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import javax.swing.SwingUtilities;
+import view.HomeView;
 
 public class Main {
     public static void main(String[] args) {
